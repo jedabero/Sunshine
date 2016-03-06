0c47d3495b9eec9020177437befe0edd47c3e1c7
jedabero <jedabero@hotmail.com>
2016-03-06 09:36:39 -0500

    Added list fragment with dummy content

--- app/src/main/java/com/jedabero/sunshine/MainActivity.java
+++ app/src/main/java/com/jedabero/sunshine/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.jedabero.sunshine.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements
+    ItemFragment.OnListFragmentInteractionListener {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, ItemFragment.newInstance(2)).commit();
+        }
     }
 
     @Override
@@ -48,5 +59,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.d(TAG, "onListFragmentInteraction: " + item);
     }
 }
